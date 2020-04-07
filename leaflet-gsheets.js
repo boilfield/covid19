7c8f68e05a4f875eb921b0a4db6713657ebecef2
nr072 <26773186+nr072@users.noreply.github.com>
2020-04-07 20:40:34 +0600

    commented unnecessary code

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -146,16 +146,16 @@ var bounds_group = new L.featureGroup([]);
 
 
 
-    function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
-}
+//     function getColor(d) {
+//     return d > 1000 ? '#800026' :
+//            d > 500  ? '#BD0026' :
+//            d > 200  ? '#E31A1C' :
+//            d > 100  ? '#FC4E2A' :
+//            d > 50   ? '#FD8D3C' :
+//            d > 20   ? '#FEB24C' :
+//            d > 10   ? '#FED976' :
+//                       '#FFEDA0';
+// }
 
 function style(feature) {
     return {
@@ -168,5 +168,5 @@ function style(feature) {
     };
 }
 
-L.geoJson(geojsonStates, {style: style}).addTo(map);
+// L.geoJson(geojsonStates, {style: style}).addTo(map);
 
