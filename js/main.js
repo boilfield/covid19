f4bfcc2cfe4509247c65c7fecd2133ad1a8681f2
nr072 <26773186+nr072@users.noreply.github.com>
2020-04-29 20:17:14 +0600

    keep map legend initially collapsed for screen width < 600px

--- js/main.js
+++ js/main.js
@@ -24,12 +24,11 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-let legend_toggler = document.getElementById("legend_toggler");
-legend_toggler.addEventListener("click", function () {
+function toggle_map_legend(showLegend) {
     let legend = document.querySelector(".info.legend .legend-cont");
     let toggler_label = document.getElementById("legend_toggler_label");
-    let css_tr = legend_toggler.style.transform;
-    if (!css_tr || css_tr === "rotate(90deg)") {
+
+    if (showLegend) {
         legend.style.maxHeight = 0;
         legend_toggler.style.transform = "rotate(-90deg)";
         setTimeout(function () {
@@ -40,4 +39,23 @@ legend_toggler.addEventListener("click", function () {
         legend.style.maxHeight = "100vh";
         legend_toggler.style.transform = "rotate(90deg)";
     }
+}
+
+let legend_toggler = document.getElementById("legend_toggler");
+legend_toggler.addEventListener("click", function () {
+    let css_tr = legend_toggler.style.transform;
+
+    // Show or hide/collapse map legend
+    if (!css_tr || css_tr === "rotate(90deg)") {
+        toggle_map_legend(1);
+    } else {
+        toggle_map_legend(0);
+    }
 });
+
+// Keep map legend initially collapsed for screen width < 600px
+(function () {
+    if (window.innerWidth < 600) {
+        toggle_map_legend(1);
+    }
+})();
