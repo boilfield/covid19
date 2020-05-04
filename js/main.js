d5bf77a0cdeeea7364cfd5bd98251601bd2b6fd8
nr072 <26773186+nr072@users.noreply.github.com>
2020-05-04 21:46:01 +0600

    refactor: reworded map legend toggle function's conditions

--- js/main.js
+++ js/main.js
@@ -18,15 +18,15 @@ function toggle_map_legend(showLegend) {
     let toggler_label = document.getElementById("legend_toggler_label");
 
     if (showLegend) {
+        toggler_label.style.maxHeight = 0;
+        legend.style.maxHeight = "100vh";
+        legend_toggler.style.transform = "rotate(90deg)";
+    } else {
         legend.style.maxHeight = 0;
         legend_toggler.style.transform = "rotate(-90deg)";
         setTimeout(function () {
             toggler_label.style.maxHeight = "100vh";
         }, 500);
-    } else {
-        toggler_label.style.maxHeight = 0;
-        legend.style.maxHeight = "100vh";
-        legend_toggler.style.transform = "rotate(90deg)";
     }
 }
 
@@ -37,9 +37,9 @@ if (legend_toggler) {
 
         // Show or hide/collapse map legend
         if (!css_tr || css_tr === "rotate(90deg)") {
-            toggle_map_legend(1);
-        } else {
             toggle_map_legend(0);
+        } else {
+            toggle_map_legend(1);
         }
     });
 }
